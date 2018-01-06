5befe0ebb5022f71cca5edb0d731de385876a16a
Alex Nichol <unixpickle@gmail.com>
2018-01-06 13:41:39 -0800

    moved darwin constants to own file

--- tunnet/utun_darwin.go
+++ tunnet/utun_darwin.go
@@ -16,23 +16,9 @@ import (
 )
 
 const (
-	pfSystem            = 32
-	sysprotoControl     = 2
-	afSysControl        = 2
-	ioctlCTLIOCGINFO    = 0xc0644e03
-	ioctlSIOCGIFMTU     = 0xc0206933
-	ioctlSIOCSIFMTU     = 0x80206934
-	ioctlSIOCGIFADDR    = 0xc0206921
-	ioctlSIOCGIFDSTADDR = 0xc0206922
-	ioctlSIOCGIFNETMASK = 0xc0206925
-	ioctlSIOCDIFADDR    = 0x80206919
-	ioctlSIOCAIFADDR    = 0x8040691a
-	utunOptIfname       = 2
-
-	utunControl = "com.apple.net.utun_control"
+	utunOptIfname = 2
+	utunControl   = "com.apple.net.utun_control"
 )
-
-var systemByteOrder = binary.LittleEndian
 
 // MakeTunnel creates a new tunnel interface.
 func MakeTunnel() (Tunnel, error) {
